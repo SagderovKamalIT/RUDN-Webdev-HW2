29f94a3f959ab822581f1b0d4601dbfa5fbaba16
Kamal Sagderov <sagderovkamal.it@gmail.com>
2026-01-13 01:44:27 +0300

    created taskService with fetchTasks and JSONPlaceholder mapping

---
+++ src/services/taskService.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { Task, TaskStatus } from "../types/task";
+
+interface JSONPlaceholderTodo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const API_BASE_URL = "https://jsonplaceholder.typicode.com";
+
+const mapTodoToTask = (todo: JSONPlaceholderTodo): Task => {
+  const status: TaskStatus = todo.completed ? 2 : 0;
+  
+  return {
+    id: todo.id,
+    title: todo.title,
+    description: null,
+    createdAt: new Date(),
+    status,
+  };
+};
+
+export const fetchTasks = async (): Promise<Task[]> => {
+  const response = await axios.get<JSONPlaceholderTodo[]>(
+    `${API_BASE_URL}/todos`
+  );
+  return response.data.map(mapTodoToTask);
+};

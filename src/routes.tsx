b86fd56eb92d21b5b07c99379bf85181cc08ebdc
Kamal Sagderov <sagderovkamal.it@gmail.com>
2026-01-13 15:50:43 +0300

    add root route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,13 @@
+import { Navigate } from "react-router";
 import BoardPage from "./pages/BoardPage/BoardPage";
 import CreateTaskPage from "./pages/CreateTaskPage/CreateTaskPage";
 import TaskPage from "./pages/TaskPage/TaskPage";
 
 export const routes = [
+  {
+    path: "/",
+    element: <Navigate to="/board" replace />,
+  },
   {
     path: "/board",
     element: <BoardPage />,
